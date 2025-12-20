9e1b33dcd133103d1c06d5b107f39d3459804b6e
GoraniHine <chlalstlr561@daum.net>
2025-12-20 20:19:27 +0900

    B11650_좌표 정렬하기

---
+++ c++baekjoon/B11650.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n = 0;
+    cin >> n;
+
+    //vector<int> A(num);
+    vector<vector<int>> A(n, vector<int>(2));
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> A[i][0] >> A[i][1];
+    }
+
+    sort(A.begin(), A.end());
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i][0] << " " << A[i][1] << "\n";
+    }
+
+    return 0;
+}
